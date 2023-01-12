e22047fe8f00b04151f5ee6dff2eeb2cf8875013
Abbas Hussain <abbashussain2399@gmail.com>
2023-01-12 16:55:29 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2ea98ba5f0msh4f2965a017f4ac1p16e6d6jsn8736051e9dbb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3533df9bee539dbbe40a6eb20aaa18c1"; // enter your key from openweather API
+
+
+
